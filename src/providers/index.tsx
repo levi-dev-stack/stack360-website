9ea920afc45f2b9cc8e83a7e9ad97499e6736242
levi-dev-stack <nofil.zahid@teamstack360.co>
2026-07-07 17:21:02 +0500

    test: [husky and lint-staged] 4

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,7 +2,6 @@ import type { ReactComponentChildren } from '@/types/component';
 import ToastProvider from './toast-provider';
 
 const Providers = ({ children }: ReactComponentChildren) => {
-  console.log('initial log');
   return (
     <>
       {children}
