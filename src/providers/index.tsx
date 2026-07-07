3424402d8586556b80fc7e93557454ce5f3365f5
levi-dev-stack <nofil.zahid@teamstack360.co>
2026-07-07 17:18:07 +0500

    test: husky1

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,6 +2,7 @@ import type { ReactComponentChildren } from '@/types/component';
 import ToastProvider from './toast-provider';
 
 const Providers = ({ children }: ReactComponentChildren) => {
+  console.log('initial log');
   return (
     <>
       {children}
