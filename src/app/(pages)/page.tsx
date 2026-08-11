353259f8676fb806d3368af5edc10a56435fab6a
levi-dev-stack <nofil.zahid@teamstack360.co>
2026-08-11 17:09:42 +0500

    feat: replace PremiumNavbar with NavbarLoader and remove unused loading components

--- src/app/(pages)/page.tsx
+++
@@ -1,8 +0,0 @@
-import LandingPage from '@/components/pages/landing';
-import { seo } from '@/constants/seo';
-
-export const metadata = seo.home;
-
-export default function Page() {
-  return <LandingPage />;
-}
